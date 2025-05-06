e67c8f8bb0c6d0744bf0656d8910a6ac3b89a014
8Wenders8 <matej.volansky15@gmail.com>
2025-05-06 15:20:50 +0200

    Fix detect script

--- yolo/detect.sh
+++ yolo/detect.sh
@@ -1,15 +1,37 @@
 #!/usr/bin/env bash
+set -e
+
+# args
+IN_IMG="$1"
+OUT_IMG="$2"
+
 cd /opt/darknet || exit 1
 
+# model files
 DATA="cfg/coco.data"
 CFG="cfg/yolov3.cfg"
 WEIGHTS="yolov3.weights"
 
+# run inference
 if command -v nvidia-smi &>/dev/null && \
    nvidia-smi --query-gpu=name --format=csv,noheader | grep . &>/dev/null; then
-  echo "[entrypoint] GPU found – running with CUDA…"
-  exec ./darknet detector test "$DATA" "$CFG" "$WEIGHTS" "$@" -ext_output -dont_show
+  echo "[entrypoint] GPU found – running CUDA"
+  ./darknet detector test \
+    "$DATA" "$CFG" "$WEIGHTS" \
+    "$IN_IMG" \
+    -dont_show -ext_output > /dev/null
 else
-  echo "[entrypoint] No GPU – falling back to CPU."
-  exec ./darknet detector test "$DATA" "$CFG" "$WEIGHTS" -nogpu "$@" -ext_output -dont_show
+  echo "[entrypoint] No GPU – falling back to CPU"
+  ./darknet detector test \
+    "$DATA" "$CFG" "$WEIGHTS" \
+    "$IN_IMG" \
+    -nogpu -dont_show -ext_output > /dev/null
 fi
+
+if [ ! -f predictions.jpg ]; then
+  echo "ERROR: predictions.jpg not found!" >&2
+  exit 1
+fi
+
+cp predictions.jpg "$OUT_IMG"
+
